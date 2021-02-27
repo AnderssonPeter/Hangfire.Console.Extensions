93ebc102a1e09424d39049c0c5fc57fe0ec1f566
Sam De beul <SDEBEUL@volvocars.com>
2021-02-27 16:40:42 +0100

    Cleanup remove old duplicated class.

--- Hangfire.Console.Extensions.Serilog/PerformContextValue.cs
+++ Hangfire.Console.Extensions.Serilog/PerformContextValue.cs
@@ -5,20 +5,6 @@ using Serilog.Events;
 
 namespace Hangfire.Console.Extensions.Serilog
 {
-    internal class PerformContextValueXX : LogEventPropertyValue
-    {
-        // The context attached to this property value
-        public PerformContext PerformContext { get; set; }
-
-        /// <inheritdoc />
-        public override void Render(TextWriter output, string format = null, IFormatProvider formatProvider = null)
-        {
-            // How the value will be rendered in Json output, etc.
-            // Not important for the function of this code..
-            output.Write(PerformContext.BackgroundJob.Id);
-        }
-    }
-
     internal class PerformContextValue : ScalarValue
     {
         public PerformContextValue(object value) : base(value)
