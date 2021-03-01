2d4db8912bc49ffd4082c99f1f1b654847f5878f
Peter Andersson <AnderssonPeter@users.noreply.github.com>
2021-03-01 17:16:28 +0100

    Change how HangFireJob is sent to serilog

--- Hangfire.Console.Extensions.Serilog/HangfireConsoleSerilogEnricher.cs
+++ Hangfire.Console.Extensions.Serilog/HangfireConsoleSerilogEnricher.cs
@@ -1,4 +1,8 @@
-﻿using Serilog.Core;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Hangfire.Server;
+using Serilog.Core;
 using Serilog.Events;
 
 namespace Hangfire.Console.Extensions.Serilog
@@ -11,6 +15,30 @@ namespace Hangfire.Console.Extensions.Serilog
         {
         }
 
+        private static StructureValue CreateValue(PerformingContext performingContext)
+        {
+            var properties = new List<LogEventProperty>()
+            {
+                new LogEventProperty("Id", new ScalarValue(performingContext.BackgroundJob.Id)),
+                new LogEventProperty("CreatedAt", new ScalarValue(performingContext.BackgroundJob.CreatedAt))
+            };
+            if (performingContext.BackgroundJob.Job != null)
+            {
+                properties.Add(new LogEventProperty("Type", new ScalarValue(performingContext.BackgroundJob.Job.Method.DeclaringType.Name)));
+                properties.Add(new LogEventProperty("Method", new ScalarValue(performingContext.BackgroundJob.Job.Method.Name)));
+                properties.Add(new LogEventProperty("Arguments", new SequenceValue(performingContext.BackgroundJob.Job.Args.Select(x => GetScalarValue(x)))));
+            }
+            return new StructureValue(properties);
+        }
+
+        private static ScalarValue GetScalarValue(object value)
+        {
+            if (value != null && !value.GetType().IsPrimitive)
+            {
+                value = value.ToString();
+            }
+            return new ScalarValue(value);
+        }
 
         /// <inheritdoc />
         public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
@@ -19,10 +47,8 @@ namespace Hangfire.Console.Extensions.Serilog
             if (context == null)
                 return;
             // Create property value with PerformContext and put as "PerformContext"
-            var prop = new LogEventProperty(
-                "PerformContext", new PerformContextValue(null) { PerformContext = context }
-            );
-            logEvent.AddOrUpdateProperty(prop);
+            var property = new LogEventProperty("HangFireJob", CreateValue(context));
+            logEvent.AddOrUpdateProperty(property);
         }
     }
 }
